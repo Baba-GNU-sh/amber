7c7b792aaa8eb29e7c2d5acced0b8ece346dcfc9
Steve Golton <stevegolton@gmail.com>
2022-01-01 22:47:03 +0000

    Use geom shader to transform lines strip to triangle strip

--- geometry.glsl
+++ geometry.glsl
@@ -4,14 +4,20 @@
 layout (lines) in;
 
 // Standard fare for drawing lines
-layout (line_strip, max_vertices = 2) out;
+layout (triangle_strip, max_vertices = 4) out;
 
 void main (void) {
 
-  gl_Position = gl_in[0].gl_Position; // First vertex in the line
+  gl_Position = gl_in[0].gl_Position + vec4(0.0, 0.1, 0.0, 0.0);
   EmitVertex();
 
-  gl_Position = gl_in[1].gl_Position + vec4(-0.1, 0.0, 0.0, 0.0);; // Second vertex in the line
+  gl_Position = gl_in[0].gl_Position + vec4(0.0, -0.1, 0.0, 0.0);
+  EmitVertex();
+
+  gl_Position = gl_in[1].gl_Position + vec4(0.0, 0.1, 0.0, 0.0);
+  EmitVertex();
+
+  gl_Position = gl_in[1].gl_Position + vec4(0.0, -0.1, 0.0, 0.0);
   EmitVertex();
 
   EndPrimitive();
