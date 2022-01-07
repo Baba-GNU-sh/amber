084197efc7ac3e5aeece2973faf213ef50abc7b0
Steve Golton <stevegolton@gmail.com>
2022-01-07 07:42:26 +0000

    Colour different line triangle segments seperately

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,9 +1,9 @@
 #version 330 core
 
 out vec4 FragColor;
+flat in vec3 fColor;
 
 void main()
 {
-    // Output a nice orange
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    FragColor = vec4(fColor, 1.0);
 };
--- shaders/geometry.glsl
+++ shaders/geometry.glsl
@@ -10,6 +10,8 @@ layout (lines) in;
 // We could probably also do this with a quad
 layout (triangle_strip, max_vertices = 4) out;
 
+flat out vec3 fColor;
+
 void main (void)
 {
     // Work out the angle between this point and the next point
@@ -21,12 +23,17 @@ void main (void)
     vec2 offset = vec2(line_thickness.x * cos(angle), line_thickness.y * -sin(angle));
 
     // Emit the 4 corners of the rectangle that defines the line
+    // Colour the first triangle orange
+    fColor = vec3(1.0f, 0.5f, 0.2f);
     gl_Position = gl_in[0].gl_Position + vec4(offset, 0.0, 0.0);
     EmitVertex();
     gl_Position = gl_in[0].gl_Position + vec4(-offset, 0.0, 0.0);
     EmitVertex();
     gl_Position = gl_in[1].gl_Position + vec4(offset, 0.0, 0.0);
     EmitVertex();
+
+    // Colour the second triangle green - the "flat" qualifier on the output means the triangle is entirely this colour
+    fColor = vec3(0.5f, 1.0f, 0.2f);
     gl_Position = gl_in[1].gl_Position + vec4(-offset, 0.0, 0.0);
     EmitVertex();
 
