1d4799276eaf23dc56d8eb6032a4a7aa14f2c3be
Steve Golton <stevegolton@gmail.com>
2022-01-07 18:50:58 +0000

    Refactored out line normal function from geometry shader

--- shaders/geometry.glsl
+++ shaders/geometry.glsl
@@ -12,15 +12,18 @@ layout (triangle_strip, max_vertices = 4) out;
 
 flat out vec3 fColor;
 
+vec2 get_line_normal(vec4 line_start, vec4 line_end)
+{
+    // Work out the angle between this point and the next point
+    vec4 delta = line_start - line_end;
+    float angle = atan(delta.x, delta.y);
+    return vec2(line_thickness.x * cos(angle), line_thickness.y * -sin(angle));
+}
+
 void main (void)
 {
     // Work out the angle between this point and the next point
-    vec4 delta = gl_in[1].gl_Position - gl_in[0].gl_Position;
-    float angle = atan(delta.x, delta.y);
-
-    // This offset describes the offset from the original point to the top of the rectangle
-    // Negate it to get the lower offset
-    vec2 offset = vec2(line_thickness.x * cos(angle), line_thickness.y * -sin(angle));
+    vec2 offset = get_line_normal(gl_in[1].gl_Position, gl_in[0].gl_Position);
 
     // Emit the 4 corners of the rectangle that defines the line
     // Colour the first triangle orange
