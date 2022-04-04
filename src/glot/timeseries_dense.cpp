7035b78e6d902b9a4ee42aa527c16dc8b63c791b
Steve Golton <stevegolton@gmail.com>
2022-04-04 17:42:01 +0100

    Added some more benchmarks for timeseries

--- src/glot/timeseries_dense.cpp
+++ src/glot/timeseries_dense.cpp
@@ -1,10 +1,11 @@
-#include <algorithm>
-
 #include "timeseries_dense.hpp"
+
+#include <algorithm>
+#include <limits>
+#include <numeric>
 
 TimeSeriesDense::TimeSeriesDense(double start, double interval) : _interval(interval), _start(start)
 {
-    //
 }
 
 std::size_t TimeSeriesDense::get_samples(TSSample *samples,
@@ -36,6 +37,9 @@ std::size_t TimeSeriesDense::get_samples(TSSample *samples,
             auto index_first = static_cast<int>((first - _start) / _interval);
             auto index_last = static_cast<int>((last - _start) / _interval);
 
+            auto iter_first = _data.begin() + index_first;
+            auto iter_last = _data.begin() + index_last;
+
             if (index_first == index_last)
             {
                 sample.average = _data[index_first];
@@ -44,19 +48,10 @@ std::size_t TimeSeriesDense::get_samples(TSSample *samples,
             }
             else
             {
-                double average = 0.0;
-                double max = -1000000;
-                double min = 1000000;
-                for (int i = index_first; i < index_last; i++)
-                {
-                    average += _data[i];
-                    max = std::max(max, _data[i]);
-                    min = std::min(min, _data[i]);
-                }
-                average /= (index_last - index_first);
-                sample.average = average;
-                sample.min = min;
-                sample.max = max;
+                const auto results = _reduce(iter_first, iter_last);
+                sample.average = std::get<0>(results);
+                sample.min = std::get<1>(results);
+                sample.max = std::get<2>(results);
             }
         }
 
@@ -83,11 +78,36 @@ std::pair<double, double> TimeSeriesDense::get_span() const
 void TimeSeriesDense::push_sample(double value)
 {
     std::lock_guard<std::recursive_mutex> _(_mut);
-    _data.insert(_data.end(), value);
+    _data.push_back(value);
 }
 
-void TimeSeriesDense::push_samples(const std::vector<double> &value)
+void TimeSeriesDense::push_samples(const std::vector<double> &data)
 {
     std::lock_guard<std::recursive_mutex> _(_mut);
-    _data.insert(_data.end(), value.begin(), value.end());
+    _data.insert(_data.end(), data.begin(), data.end());
 }
+
+std::tuple<double, double, double> TimeSeriesDense::_reduce(
+    std::vector<double>::const_iterator begin, std::vector<double>::const_iterator end) const
+{
+    double min = std::numeric_limits<double>::max();
+    double max = std::numeric_limits<double>::lowest();
+    double sum = 0.0;
+    for (auto iter = begin; iter != end; iter++)
+    {
+        min = std::min(min, *iter);
+        max = std::max(max, *iter);
+        sum += *iter;
+    }
+    const auto average = sum / (end - begin);
+    return std::make_tuple(average, min, max);
+}
+
+// std::tuple<double, double, double> TimeSeriesDense::_reduce(
+//     std::vector<double>::const_iterator begin, std::vector<double>::const_iterator end) const
+// {
+//     const auto len = (end - begin);
+//     auto average = std::reduce(std::execution::seq, begin, end, 0.0) / len;
+//     auto minmax = std::minmax_element(std::execution::seq, begin, end);
+//     return std::make_tuple(average, *minmax.first, *minmax.second);
+// }
--- src/glot/timeseries_dense.hpp
+++ src/glot/timeseries_dense.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <mutex>
+#include <tuple>
 #include <vector>
 
 #include "timeseries.hpp"
 
 /**
  * @brief A densely packed time series with a fixed data rate.
- * This time series is useful if you 
+ * This time series is useful if you
  */
 class TimeSeriesDense : public TimeSeries
 {
@@ -20,14 +21,12 @@ class TimeSeriesDense : public TimeSeries
                             std::size_t num_samples) const override;
     TSSample get_sample(double timestamp, double bin_width) override;
     std::pair<double, double> get_span() const override;
-
-    /**
-     * @brief Adds a new sample to the timeseries.
-     * @param value The value of the sample to add.
-     */
     void push_sample(double value);
     void push_samples(const std::vector<double> &value);
 
+  private:
+    std::tuple<double, double, double> _reduce(std::vector<double>::const_iterator,
+                                               std::vector<double>::const_iterator) const;
     mutable std::recursive_mutex _mut;
     std::vector<double> _data;
     double _interval;
